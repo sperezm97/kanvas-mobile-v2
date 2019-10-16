45ca04bd31d3160ca9547bea388638ec96216216
Sebastian Perez <sperezm97@gmail.com>
2019-10-16 14:06:06 -0400

    Create index.tsx

---
+++ src/components/title-bar/index.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import {
+  Header,
+  Body as nbBody,
+  Left as nbLeft,
+  Right as nbRight,
+  View,
+} from 'native-base';
+import styled from 'styled-components';
+
+const Body = styled(nbBody)`
+  flex: 1 0px;
+  min-width: 120px;
+`;
+const Left = styled(nbLeft)`
+  flex: 1 0px;
+`;
+const Right = styled(nbRight)`
+  flex: 1 0px;
+`;
+
+interface ITitleBar {
+  right?: JSX.Element;
+  body?: JSX.Element;
+  left?: JSX.Element;
+  barStyle?: 'light-content' | 'dark-content';
+  noShadow?: boolean;
+}
+const TitleBar = ({ right, body, left, barStyle, noShadow }: ITitleBar) => {
+  return (
+    <Header iosBarStyle={barStyle} noShadow={noShadow}>
+      <Right>{right}</Right>
+      <Body>{body}</Body>
+      <Left>{left}</Left>
+    </Header>
+  );
+};
+
+TitleBar.defaultsProps = {
+  right: <View />,
+  body: <View />,
+  left: <View />,
+  barStyle: 'light-content',
+  noShadow: false,
+};
+export default TitleBar;
