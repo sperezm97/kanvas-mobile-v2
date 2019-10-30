31587fb1c6650e33c2c308f18fd5621bde43577b
Sebastian Perez <sperezm97@gmail.com>
2019-10-30 17:46:51 -0400

    upgrade rn version

--- src/modules/Login/sagas.ts
+++ src/modules/Login/sagas.ts
@@ -1,12 +1,13 @@
 import { put, call, takeLatest } from 'redux-saga/effects';
 import { SIGN_IN, ERROR_SIGN_IN } from './actions';
 import api from '@config/api';
+
 // call generators functions
 export function* signIn(data: object) {
   try {
     yield call(api.signIn, data);
   } catch (error) {
-    yield put({ type: ERROR_SIGN_IN, error });
+    yield put({ type: ERROR_SIGN_IN, payload: error });
   }
 }
 // watchers
